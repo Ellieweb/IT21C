f7347c87ba48c475e51dda0b5ff6489ebdff8e5b
Leanne Tudtud <tudtudleanne@gmail.com>
2025-09-10 23:50:02 +0800

    First commit: added student variables

---
+++ tudtudleaLife.js
@@ -0,0 +1,10 @@
+// Step 1
+let name = "Leanne Tudtud";
+let course = "BS Information Technology";
+let yearLevel = "2nd Year";
+
+console.log("Name:", name);
+console.log("Course:", course);
+console.log("Year Level:", yearLevel);
+
+
