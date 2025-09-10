33dc2e378584263958c27e3603677b9dc2ceecb6
Leanne Tudtud <tudtudleanne@gmail.com>
2025-09-11 00:57:53 +0800

    Added operators for daily routine

--- tudtudLife.js
+++ tudtudLife.js
@@ -7,3 +7,9 @@ console.log("Name:", name);
 console.log("Course:", course);
 console.log("Year Level:", yearLevel);
 
+// Step 2: Operators
+let hoursOfSleep = 7;
+let hoursOfStudy = 4;
+
+let totalHours = hoursOfSleep + hoursOfStudy;
+console.log("Total hours of daily routine:", totalHours);
